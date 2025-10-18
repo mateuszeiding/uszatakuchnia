fb4f08ddee3aa3a2d26af9857b496267097d67be
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-10-18 19:08:06 +0200

    Enum endpoint

---
+++ http/response.go
@@ -0,0 +1,19 @@
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, status int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(v)
+}
+
+func Error(w http.ResponseWriter, status int, msg string) {
+	type errResp struct {
+		Error string `json:"error"`
+	}
+	JSON(w, status, errResp{Error: msg})
+}
