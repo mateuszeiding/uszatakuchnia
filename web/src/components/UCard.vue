c0fc82e763d6047deb3495f134a61419f8777741
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-10-22 00:59:09 +0200

    A bit better cards

--- web/src/components/UCard.vue
+++ web/src/components/UCard.vue
@@ -8,10 +8,10 @@ const props = defineProps<{
 }>()
 
 const badges: { name: string; arr: IType<PropsOf<typeof UBadge>['val']>[] }[] = [
-  {
-    name: 'Aromat',
-    arr: props.ingredient.aromas,
-  },
+  // {
+  //   name: 'Aromat',
+  //   arr: props.ingredient.aromas,
+  // },
   {
     name: 'Smak',
     arr: props.ingredient.tastes,
@@ -19,42 +19,38 @@ const badges: { name: string; arr: IType<PropsOf<typeof UBadge>['val']>[] }[] = 
 ]
 </script>
 <template>
-  <div class="container-cp border radius-4 px-5 py-4 w-100 h-100 bg-brown-lavender-blush">
-    <div class="row">
-      <div class="col">
-        <div class="d-flex justify-content-between align-items-center pb-2">
-          <div class="fs-xl">
+  <div class="container-cp border radius-4 pe-5 pb-4 w-100 h-100 bg-purple-magnolia">
+    <div class="row pb-4">
+      <div class="col-6 ps-0">
+        <div class="image-wrapper p-relative">
+          <img class="radius-top-s-4 radius-bot-e-4" :src="ingredient.image.urls.small" />
+          <a
+            class="fs-xs p-absolute left-0 bottom-0 bg-purple-magnolia py-1 px-3 radius-top-e-2 tx-violet-french-violet"
+            :href="ingredient.image.author.profileURL"
+            >{{ ingredient.image.author.name ?? ingredient.image.author.username }}</a
+          >
+        </div>
+      </div>
+      <div class="col-6 pt-4">
+        <div>
+          <div class="fs-xl pb-2">
             {{ ingredient.name }}
           </div>
           <u-badge class="d-inline-flex" :val="ingredient.type.code" :name="ingredient.type.name" />
+          <template v-for="v in badges" :key="v.name">
+            <div class="pt-4">
+              <!-- <div class="pb-2 text-end">{{ v.name }}</div> -->
+              <div class="d-flex pt-3 border-top gap-3 justify-content- flex-wrap">
+                <u-badge v-for="val in v.arr" :key="val.code" :val="val.code" :name="val.name" />
+              </div>
+            </div>
+          </template>
         </div>
       </div>
     </div>
+
     <div class="row">
-      <div class="col-6">
-        <div class="py-4">
-          <div class="image-wrapper">
-            <img class="radius-5" :src="ingredient.image.urls.small" />
-          </div>
-          <div class="fs-xs">
-            <span>Photo by </span>
-            <a :href="ingredient.image.author.profileURL">{{
-              ingredient.image.author.name ?? ingredient.image.author.username
-            }}</a>
-            <span> on <b>Unsplash</b></span>
-          </div>
-        </div>
-      </div>
-      <div class="col-6">
-        <template v-for="v in badges" :key="v.name">
-          <div class="py-2">
-            <div class="pb-2 text-end">{{ v.name }}</div>
-            <div class="d-flex gap-3 justify-content-end flex-wrap">
-              <u-badge v-for="val in v.arr" :key="val.code" :val="val.code" :name="val.name" />
-            </div>
-          </div>
-        </template>
-      </div>
+      <div class="col"></div>
     </div>
   </div>
 </template>
