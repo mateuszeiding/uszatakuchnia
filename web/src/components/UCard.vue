fea124d3be8bd75cfb150025cf35019d96fba3dd
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-10-22 01:03:49 +0200

    And a bit bit more?

--- web/src/components/UCard.vue
+++ web/src/components/UCard.vue
@@ -19,13 +19,13 @@ const badges: { name: string; arr: IType<PropsOf<typeof UBadge>['val']>[] }[] = 
 ]
 </script>
 <template>
-  <div class="container-cp border radius-4 pe-5 pb-4 w-100 h-100 bg-purple-magnolia">
-    <div class="row pb-4">
+  <div class="container-cp border radius-4 pe-5 w-100 h-100 bg-purple-magnolia">
+    <div class="row">
       <div class="col-6 ps-0">
         <div class="image-wrapper p-relative">
-          <img class="radius-top-s-4 radius-bot-e-4" :src="ingredient.image.urls.small" />
+          <img class="radius-top-s-4 radius-bot-s-4" :src="ingredient.image.urls.small" />
           <a
-            class="fs-xs p-absolute left-0 bottom-0 bg-purple-magnolia py-1 px-3 radius-top-e-2 tx-violet-french-violet"
+            class="fs-xs p-absolute right-0 bottom-0 bg-purple-magnolia py-1 px-3 radius-top-s-2 tx-violet-french-violet"
             :href="ingredient.image.author.profileURL"
             >{{ ingredient.image.author.name ?? ingredient.image.author.username }}</a
           >
@@ -40,7 +40,7 @@ const badges: { name: string; arr: IType<PropsOf<typeof UBadge>['val']>[] }[] = 
           <template v-for="v in badges" :key="v.name">
             <div class="pt-4">
               <!-- <div class="pb-2 text-end">{{ v.name }}</div> -->
-              <div class="d-flex pt-3 border-top gap-3 justify-content- flex-wrap">
+              <div class="d-flex pt-4 border-top gap-3 justify-content- flex-wrap">
                 <u-badge v-for="val in v.arr" :key="val.code" :val="val.code" :name="val.name" />
               </div>
             </div>
