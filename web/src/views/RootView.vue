a68e5e93c3784df81d7331108132964e1803931b
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-11-18 00:17:23 +0100

    Base form inputs

--- web/src/views/RootView.vue
+++ web/src/views/RootView.vue
@@ -1,4 +1,11 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import UInput from '@/components/form/UInput.vue'
+</script>
 <template>
-  <div></div>
+  <div>
+    <UInput name="test" label="test" type="text" />
+    <UInput name="test" label="test" type="radio" />
+    <UInput name="test" label="test" type="checkbox" />
+    <UInput name="test" label="test" type="checkbox" switch />
+  </div>
 </template>
