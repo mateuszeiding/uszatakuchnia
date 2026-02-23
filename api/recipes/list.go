75bf1babbca31d349e5c17f5a85e0bb075e37145
MATEUSZ EIDING <eidingmateusz@gmail.com>
2026-02-23 22:41:06 +0100

    recipe list endpoint

---
+++ api/recipes/list.go
@@ -0,0 +1,29 @@
+package ingredients
+
+import (
+	"net/http"
+
+	"uszatakuchnia/db"
+	"uszatakuchnia/db/entities"
+	"uszatakuchnia/db/mappers"
+	resp "uszatakuchnia/http"
+)
+
+func List(w http.ResponseWriter, r *http.Request) {
+	if resp.HandleCORS(w, r) {
+		return
+	}
+	conn := db.DB()
+
+	var list []entities.Recipe
+	if err := conn.
+		Preload("RecipeStep").
+		Preload("RecipeIngredient").
+		Find(&list).Error; err != nil {
+		resp.JSON(w, http.StatusInternalServerError, map[string]any{"error": err.Error()})
+		return
+	}
+
+	dtos := mappers.MapRecipeArrayToDto(list)
+	resp.JSON(w, http.StatusOK, dtos)
+}
